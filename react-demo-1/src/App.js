8c2d1a6a197e0908c993b70f527cc652fbb09720
Mianxiong <757134184@qq.com>
2023-02-06 17:34:13 +0800

    ShouldComponentUpdate和PureComponent

--- react-demo-1/src/App.js
+++ react-demo-1/src/App.js
@@ -1,51 +1,119 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      爸爸
-      <Son />
-    </div>
-  );
-}
-
-class Son extends React.Component {
-  constructor() {
-    super();
+//PureComponent发现对象不一样，但里面值是一样的,都是一样的就不render
+//PureComponent在render之前，对新旧state进行对比，对比只对比一层，最外面那一层，不会递归
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
     this.state = {
-      n: 0,
+      n: 1,
     };
   }
-  add() {
-    //this.state.n += 1
-    this.setState({ n: this.state.n + 1 });
-  }
+  onClick = () => {
+    this.setState((state) => {return {
+      n: state.n - 1
+    }});
+    this.setState((state) =>  ({
+      n: state.n + 1
+    }));
+  };
+  //PureComponent发现对象不一样，但里面值是一样的,都是一样的就不render
+  // shouldComponentUpdate(newProps, newState) {
+  //   if(newState.n === this.state.n) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
   render() {
+    console.log('render了一次')
     return (
-      <div className="Son">
-        儿子 n: {this.state.n}
-        <button onClick={() => this.add()}>+1</button>
-        <Grandson />
+      <div>
+        App
+        <div>
+          {this.state.n}
+          <button onClick={this.onClick}>+1</button>
+        </div>
       </div>
     );
   }
 }
-const GrandSon = () => {
-  // const array = React.useState(0);
-  // const n = array[0];
-  // const setN = array[1];
-  //setN之后是得到一个新的n，而不是改变原有的n
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {x:1}
+//   }
+//   onClick = () => {
+//     this.setState({
+//       x:this.state.x+1
+//     })
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         App <button onClick={this.onClick}>+1</button>
+//         <B name={this.state.x}/>
+//       </div>
+//     )
+//   }
+// }
 
-  const [n, setN] = React.useState(0);
-  return (
-    <div className="Grandson">
-      孙子 n:{n}
-      <button onClick={() => setN(n + 1)}>+1</button>
-    </div>
-  );
-};
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+// class B extends React.Component {
+//   render() {
+//     return (
+//       <div>{this.props.name}</div>
+//     )
+//   }
+// }
+
+export default App;
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import "./styles.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       爸爸
+//       <Son />
+//     </div>
+//   );
+// }
+
+// class Son extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       n: 0,
+//     };
+//   }
+//   add() {
+//     //this.state.n += 1
+//     this.setState({ n: this.state.n + 1 });
+//   }
+//   render() {
+//     return (
+//       <div className="Son">
+//         儿子 n: {this.state.n}
+//         <button onClick={() => this.add()}>+1</button>
+//         <Grandson />
+//       </div>
+//     );
+//   }
+// }
+// const GrandSon = () => {
+//   // const array = React.useState(0);
+//   // const n = array[0];
+//   // const setN = array[1];
+//   //setN之后是得到一个新的n，而不是改变原有的n
+
+//   const [n, setN] = React.useState(0);
+//   return (
+//     <div className="Grandson">
+//       孙子 n:{n}
+//       <button onClick={() => setN(n + 1)}>+1</button>
+//     </div>
+//   );
+// };
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);

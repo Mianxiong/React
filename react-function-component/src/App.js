9959a476e402c260daba3018407af89a7cd9640b
Mianxiong <757134184@qq.com>
2023-02-07 12:59:56 +0800

    函数组件部分生命周期

--- react-function-component/src/App.js
+++ react-function-component/src/App.js
@@ -1,26 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function sayHi(){
-  alert('Hi')
+// const App = (props) => {
+// const [n, setN] = useState(0);
+// const [m, setM] = useState(0);
+// const onClickN = () => {
+//   setN(n + 1);
+// };
+// const onClickM = () => {
+//   setM(m + 1);
+// };
+// useEffect(() => {
+//   console.log("use effect");
+// }, []); //[]只在第一次渲染的时候调用
+
+// useEffect(() => {
+//   console.log("n或者m 变了");
+// }, [n,m]);
+// return (
+// <div>
+//   {n}
+//   <button onClick={onClickN}>n+1</button>
+//   <hr />
+//   {m}
+//   <button onClick={onClickM}>m+1</button>
+// </div>
+// );
+const X = () => {
+  const n = 1 + 1;
+  return <div>xx</div>;
+};
+const Y = () => {
+  const n = 1+1
+  return <div>y</div>
 }
+const App = (props) => {
+  const [childVisible, setChildVisible] = useState(true);
+  const hide = () => {
+    setChildVisible(false);
+  };
+  const show = () => {
+    setChildVisible(true);
+  };
+  return (
+    // <div>
+    //   {childVisible ? (
+    //     <button onClick={hide}>hide</button>
+    //   ) : (
+    //     <button onClick={show}>show</button>
+    //   )}
+    //   {childVisible ? <Child /> : null}
+    // </div>
+    <React.Fragment>
+      <X></X>
+      <Y></Y>
+    </React.Fragment>
+  );
+};
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React 也不过如此嘛</h1>
-        </header>
-        <p className="App-intro">
-          编辑 <code>src/App.js</code> 就能改变页面展示
-          <button onClick={sayHi}>点我</button>
-        </p>
-      </div> // => JS
-    );
-  }
-}
-
+const Child = (props) => {
+  useEffect(() => {
+    return () => {
+      console.log("Child销毁了");
+    };
+  });
+  return <div>Child</div>;
+};
 export default App;
